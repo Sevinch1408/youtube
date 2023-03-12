77aa3aeb68d6ed0ed624c656f2132b7101bffa8c
sevinch <sevinchqudratova23@gmail.com>
2023-03-12 05:31:23 +0500

    youtube

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
+import VedioCard from './Components/VedioCard/VedioCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Header />
+  <div class="mainBody">
+    <Sidebar/>
+    <div class="videos">
+    <div class="videos__container">
+<VedioCard/>
+      </div>
+     </div>
+  </div>
+</div>
+
   );
 }
 
---
+++ src/utils/Components.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LinkSidebar = ({linkIcon , text, link}) => {
+    // console.log(linkIcon , text, link);
+    return (
+        
+        <div className='sidebar__category'>
+            <i className='material-icons'>
+                {linkIcon}
+            </i>
+            <span>{text}</span>
+        </div>
+    );
+}
+
+export {LinkSidebar};
